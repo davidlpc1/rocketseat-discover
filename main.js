82fe9bf655da64de1a0556f46222bea89471dade
Davi Lucas <davilucasdlcreator@gmail.com>
2021-02-04 12:43:40 -0300

    :sparkles: feat square animation added

--- main.js
+++ main.js
@@ -257,6 +257,7 @@ function start(){
         function changeText(hasDarkMode){
             const p = document.querySelector('.toggle p')
             p.textContent = hasDarkMode ? 'Dark Mode' : 'Light Mode';
+            darkModeCheckbox.checked = p.textContent === "Dark Mode"
             window.NotificateUser(`Tema selecionado:${p.textContent || 'Light Mode'}`)
         }
 
@@ -292,8 +293,35 @@ function start(){
         }
     }
 
+    function Squares(){
+        const ulSquares = document.querySelector("ul.squares")
+        const random = (min , max) => Math.random() * (max - min) + min;
+
+        for(let squareIndex = 0; squareIndex <= 6;squareIndex++){
+            const li = document.createElement("li")
+
+            const size = Math.floor(random(10,120));
+            const position = random(1, 99);
+            const delay = random(5, 0.1);
+            const duration = random(24, 12);
+
+            li.style.width = `${size}px`;
+            li.style.height = `${size}px`;
+            li.style.bottom = `-${size}px`;
+
+            li.style.left = `${position}%`
+
+            li.style.animationDelay = `${delay}s`
+            li.style.animationDuration = `${duration}s`
+            li.style.animationTimingFunction = `cubic-bezier(${Math.random()},${Math.random()},${Math.random()},${Math.random()})`
+
+            ulSquares.appendChild(li)
+        }
+    }
+
     return () => {
         window.NotificateUser = Notifications;
+        Squares()
         DarkMode();
         Transactions();
         Modal();
