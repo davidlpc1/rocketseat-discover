f09fbc96703bd31a90667361dc8e750c98ec6cd9
Davi Lucas <davilucasdlcreator@gmail.com>
2021-02-04 09:35:03 -0300

    :sparkles: feat app finished

--- main.js
+++ main.js
@@ -1,13 +1,255 @@
-window.addEventListener('DOMContentLoaded',() => {
-    const ModalElement = document.querySelector('.modal-overlay')
-    const openModalButton = document.querySelector('.button.new')
-    const cancelModalUse = document.querySelector('.button.cancel')
+window.addEventListener('DOMContentLoaded',start())
 
-    const ModalFunctions = {
-        open:() => () => ModalElement.classList.add('active'),
-        close:() => () => ModalElement.classList.remove('active')
+function start(){
+    function Modal(){
+        const ModalElement = document.querySelector('.modal-overlay')
+        const openModalButton = document.querySelector('.button.new')
+        const cancelModalUse = document.querySelector('.button.cancel')
+    
+        const ModalFunctions = {
+            toggle:() => () => ModalElement.classList.toggle('active')
+        }
+    
+        openModalButton.addEventListener('click',ModalFunctions.toggle())
+        cancelModalUse.addEventListener('click',ModalFunctions.toggle())
     }
 
-    openModalButton.addEventListener('click',ModalFunctions.open() )
-    cancelModalUse.addEventListener('click',ModalFunctions.close() )
-})
+    function Transactions(){
+
+        const Utils = {
+            formatCurrency(value){
+                const signal = Number(value) < 0 ? "-" : '';
+                
+                const onlyDigitsInString = String(value).replace(/\D/g,"")
+                const formatedValue = Number(onlyDigitsInString) / 100
+                const currency = formatedValue.toLocaleString("pt-br",{
+                    style: "currency",
+                    currency:"BRL"
+                })
+
+                return signal + currency
+            }
+        }
+
+        const Storage = {
+            get(){
+                return JSON.parse(localStorage.getItem("dev.finances:transactions")) || []
+            },
+
+            set(transactions){
+                localStorage.setItem("dev.finances:transactions",JSON.stringify(transactions))
+            }
+        }
+
+        const TransactionFunctions = {
+            all: Storage.get(),
+
+            add(transaction){
+                TransactionFunctions.all.push(transaction)
+
+                App.reload();
+            },
+
+            remove(index){
+                TransactionFunctions.all.splice(index, 1);
+
+                App.reload();
+            },
+
+            incomes(){
+                let income = 0
+                TransactionFunctions.all.forEach(({ amount }) => {
+                    if(amount > 0) income += amount;
+                })
+
+                return income
+            },
+
+            expenses(){
+                let expense = 0
+                TransactionFunctions.all.forEach(({ amount }) => {
+                    if(amount < 0) expense += amount;
+                })
+
+                return expense
+            },
+
+            total(){
+                return TransactionFunctions.incomes() + TransactionFunctions.expenses()
+            }
+        }
+
+        const DOM = {
+            transactionsContainer:document.querySelector('#data-table tbody'),
+
+            addTransaction( transaction,index ){
+               const tr = document.createElement('tr');
+               tr.innerHTML = DOM.innerHtmlTransaction(transaction,index)
+               tr.dataset.index = index
+               const image = tr.querySelector(`img#remove_${index}`)
+               image.addEventListener('click',() => TransactionFunctions.remove(index))
+
+               DOM.transactionsContainer.appendChild(tr)
+
+            },
+
+            innerHtmlTransaction({ description, amount,date },index){
+                if(!description && !amount && !date ) return '';
+                const CSSClass = amount < 0 ? "expense" : "income"
+
+                const formatedAmount = Utils.formatCurrency(amount)
+
+                const html = `
+                    <td class="description">${description}</td>
+                    <td class="${CSSClass}">${formatedAmount}</td>
+                    <td class="date">${date}</td>
+                    <td>
+                        <img src="./assets/minus.svg" alt="remover transação" id="remove_${index}" class="remove_transaction">
+                    </td>
+                `;
+
+                return html;
+            },
+
+            updateBalance(){
+                document
+                    .getElementById('incomeDisplay')
+                    .innerHTML = Utils.formatCurrency(TransactionFunctions.incomes())
+
+                document
+                    .getElementById('expenseDisplay')
+                    .innerHTML = Utils.formatCurrency(TransactionFunctions.expenses())
+
+                document
+                    .getElementById('totalDisplay')
+                    .innerHTML = Utils.formatCurrency(TransactionFunctions.total())
+            },
+
+            clearTransactions(){
+                DOM.transactionsContainer.innerHTML = '';
+            }
+        }
+
+        const App = {
+            init(){
+                TransactionFunctions.all.forEach(DOM.addTransaction)
+
+                DOM.updateBalance()
+
+                Storage.set(TransactionFunctions.all)
+            },
+            reload(){
+                DOM.clearTransactions()
+                App.init()
+            }
+        }
+
+        return { TransactionFunctions,App };
+    }
+
+    function Form(TransactionFunctions){
+        const descriptionInput = document.querySelector('input#description')
+        const amountInput = document.querySelector('input#amount')
+        const dateInput = document.querySelector('input#date')
+
+        const Utils = {
+            sleep(ms) {
+                return new Promise(r => setTimeout(r,ms))
+            },
+
+            formatAmount(value){
+                return Number(value.replace(/\,\./g, "")) * 100 
+            },
+
+            formatDate(date){
+                const [ year,month,day ] = date.split("-")
+                return `${day}/${month}/${year}`
+            }
+        }
+        
+        function getValues(){
+            return {
+                description: descriptionInput.value,
+                amount: amountInput.value,
+                date: dateInput.value,
+            }
+        }
+
+        function validateFields(){
+            const { description, amount, date } = getValues()
+            if( description.trim() === "" || 
+                amount.trim() === "" || 
+                date.trim() === ""){
+                throw new Error('Por favor,preencha todos os campos')
+            }
+        }
+
+        function formatValues(){
+            const { description, amount, date } = getValues()
+
+            const formatedAmount = Utils.formatAmount(amount)
+            const formatedDate = Utils.formatDate(date)
+
+            return { 
+                description, 
+                amount:formatedAmount, 
+                date:formatedDate 
+            }
+        }
+
+        function closeModal(){
+            removeErrors()
+            document.querySelector('.button.cancel').click()
+        }
+
+        async function showErrorToUser(error){
+            const errorContainer = document.querySelector('.input-group.error p');
+            errorContainer.innerHTML = error;
+
+            const errorLine = document.querySelector('.error-line')
+            while(errorLine.style.width !== '100%'){
+                const width = Number(errorLine.style.width.replace('%', '')); 
+                await Utils.sleep(1)
+                errorLine.style.width = `${width + 1}%`;
+            }
+
+        }
+
+        async function removeErrors(){
+            const errorLine = document.querySelector('.error-line')
+            errorLine.style.width = '0%' 
+
+            const errorContainer = document.querySelector('.input-group.error p');
+            errorContainer.innerHTML = '';
+        }
+
+        function clearFields(){
+            descriptionInput.value = ""
+            amountInput.value = ""
+            dateInput.value = ""
+        }
+
+        const formElement = document.querySelector('form');
+        formElement.addEventListener('submit',event => {
+            event.preventDefault()
+            
+            try{
+                removeErrors()
+                validateFields()
+                const transaction = formatValues()
+                TransactionFunctions.add(transaction)
+                clearFields()
+                closeModal()
+            }catch(error){
+                showErrorToUser(error.message)
+            }
+        })
+    }
+
+    return () => {
+        Modal();
+        const { TransactionFunctions,App } = Transactions();
+        Form(TransactionFunctions);
+        App.init()
+    }
+}
